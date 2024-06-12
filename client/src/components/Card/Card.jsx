0e26ffcc3a7abf20c6aab2e6af39401bf1061e28
JKukielski <jankukielski1@gmail.com>
2024-06-12 12:11:36 +0200

    Card component structure - functionality & UI after Strapi imp.

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,24 @@
+import './Card.scss';
+import { Link } from 'react-router-dom';
+
+// Return to component when implementing strapi
+const Card = () => {
+  return (
+    <Link className="link" to={`/product/${item.id}`}>
+      <div className="card">
+        <div className="image">
+          <span></span>
+          <img className="mainImg" src="" alt="" />
+          <img className="secondImg" src="" alt="" />
+        </div>
+        <h2></h2>
+        <div className="prices">
+          <h3></h3>
+          <h3></h3>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
