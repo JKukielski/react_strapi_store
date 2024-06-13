5ccc61929f1912ac907b34a65f7074d82a0c955f
JKukielski <jankukielski1@gmail.com>
2024-06-13 11:25:51 +0200

    Featured products component UI

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,7 +1,20 @@
 import './FeaturedProducts.scss';
 
-const FeaturedProducts = () => {
-  return <div className="featuredProducts"></div>;
+const FeaturedProducts = ({ type }) => {
+  // Return to this component when implementing Strapi
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad quia odio
+          mollitia eius veniam neque velit ullam tempore adipisci! Ab ea culpa
+          eveniet quidem pariatur quibusdam quia nostrum sequi cumque!
+        </p>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
 };
 
 export default FeaturedProducts;
